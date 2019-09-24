9566911265485c40ed18f2734c5f2370ce1ef75d
Youngjun <youngjun@JunMacMini-2.local>
2019-09-25 06:20:45 +0900

    use SearchContext

--- src/Routes/Search/SearchPresenter.js
+++ src/Routes/Search/SearchPresenter.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import Helmet from 'react-helmet';
@@ -6,6 +6,7 @@ import Loader from 'Components/Loader';
 import Section from 'Components/Section';
 import Message from '../../Components/Message';
 import Poster from '../../Components/Poster';
+import { SearchContext } from 'context';
 
 const Container = styled.div`
     padding: 20px;
@@ -23,81 +24,78 @@ const Input = styled.input`
 `;
 
 const SearchPresenter = ({
-    movieResults,
-    tvResults,
     loading,
-    searchTerm,
     handleSubmit,
     error,
     updateTerm
-  }) => (
-    <Container>
-      <Helmet>
-        <title>Search | Nomflix</title>
-      </Helmet>
-      <Form onSubmit={handleSubmit}>
-        <Input
-          placeholder="Search Movies or TV Shows..."
-          value={searchTerm}
-          onChange={updateTerm}
-        />
-      </Form>
-      {loading ? (
-        <Loader />
-      ) : (
-        <>
-          {movieResults && movieResults.length > 0 && (
-            <Section title="Movie Results">
-              {movieResults.map(movie => (
-                // <span key={movie.id}>{movie.title}</span>
-                <Poster
-                  key={movie.id}
-                  id={movie.id}
-                  imageUrl={movie.poster_path}
-                  title={movie.original_title}
-                  rating={movie.vote_average}
-                  year={movie.release_date.substring(0, 4)}
-                  // substring 을 이용하여 원하는 만큼의 길이를 자른다.
-                  // 년도만 가져오면 되니까
-                  isMovie={true}
-                />
-              ))}
-            </Section>
-          )}
-          {tvResults && tvResults.length > 0 && (
-            <Section title="TV Show Results">
-              {tvResults.map(show => (
-                // <span key={show.id}>{show.name}</span>
-                <Poster
-                  key={show.id}
-                  id={show.id}
-                  imageUrl={show.poster_path}
-                  title={show.original_name}
-                  rating={show.vote_average}
-                  year={show.first_air_date.substring(0, 4)}
-                  // substring 을 이용하여 원하는 만큼의 길이를 자른다.
-                  // 년도만 가져오면 되니까
-                />
-              ))}
-            </Section>
-          )}
-          {error && <Message color="#e74c3c" text={error} />}
-          {tvResults &&
-            movieResults &&
-            tvResults.length === 0 &&
-            movieResults.length === 0 && (
-              <Message text="Nothing found" color="#95a5a6" />
+  }) => {
+    const { search: { searchTerm, movieResults, tvResults } } = useContext(SearchContext);
+    return (
+      <Container>
+        <Helmet>
+          <title>Search | Nomflix</title>
+        </Helmet>
+        <Form onSubmit={handleSubmit}>
+          <Input
+            placeholder="Search Movies or TV Shows..."
+            value={searchTerm}
+            onChange={updateTerm}
+          />
+        </Form>
+        {loading ? (
+          <Loader />
+        ) : (
+          <>
+            {movieResults && movieResults.length > 0 && (
+              <Section title="Movie Results">
+                {movieResults.map(movie => (
+                  // <span key={movie.id}>{movie.title}</span>
+                  <Poster
+                    key={movie.id}
+                    id={movie.id}
+                    imageUrl={movie.poster_path}
+                    title={movie.original_title}
+                    rating={movie.vote_average}
+                    year={movie.release_date.substring(0, 4)}
+                    // substring 을 이용하여 원하는 만큼의 길이를 자른다.
+                    // 년도만 가져오면 되니까
+                    isMovie={true}
+                  />
+                ))}
+              </Section>
             )}
-        </>
-      )}
-    </Container>
-);
+            {tvResults && tvResults.length > 0 && (
+              <Section title="TV Show Results">
+                {tvResults.map(show => (
+                  // <span key={show.id}>{show.name}</span>
+                  <Poster
+                    key={show.id}
+                    id={show.id}
+                    imageUrl={show.poster_path}
+                    title={show.original_name}
+                    rating={show.vote_average}
+                    year={show.first_air_date.substring(0, 4)}
+                    // substring 을 이용하여 원하는 만큼의 길이를 자른다.
+                    // 년도만 가져오면 되니까
+                  />
+                ))}
+              </Section>
+            )}
+            {error && <Message color="#e74c3c" text={error} />}
+            {tvResults &&
+              movieResults &&
+              tvResults.length === 0 &&
+              movieResults.length === 0 && (
+                <Message text="Nothing found" color="#95a5a6" />
+              )}
+          </>
+        )}
+      </Container>
+    )
+  }
 
 SearchPresenter.propTypes = {
-    movieResults: PropTypes.array,
-    tvResults: PropTypes.array,
     error: PropTypes.string,
-    searchTerm: PropTypes.string,
     loading: PropTypes.bool.isRequired,
     handleSubmit: PropTypes.func.isRequired,
     updateTerm: PropTypes.func.isRequired
